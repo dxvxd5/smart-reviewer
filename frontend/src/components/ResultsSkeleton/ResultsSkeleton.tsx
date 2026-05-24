b358d8e7f38142a8c407c9c5a0c4cb0cf133c2d5
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2026-05-24 19:26:17 +0100

    feat(results-skeleton): add loading skeleton card

---
+++ frontend/src/components/ResultsSkeleton/ResultsSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from "../Skeleton/Skeleton";
+import styles from "./ResultsSkeleton.module.css";
+
+export function ResultsSkeleton() {
+  return (
+    <div className={styles.root}>
+      <Skeleton w={92} h={92} />
+      <div className={styles.stack}>
+        <Skeleton h={12} w="40%" />
+        <Skeleton h={18} />
+        <Skeleton h={14} w="92%" />
+        <Skeleton h={14} w="76%" />
+      </div>
+    </div>
+  );
+}
